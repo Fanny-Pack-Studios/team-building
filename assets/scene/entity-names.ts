d599a34b48c6d391bf272ceb76ea6bfa0544c17a
agustincarriso <86750198+agustincarriso@users.noreply.github.com>
2025-06-12 11:09:22 -0300

    ft: add screen & admin tools

--- assets/scene/entity-names.ts
+++ assets/scene/entity-names.ts
@@ -5,5 +5,7 @@
  * Object containing all entity names in the scene for autocomplete support.
  */
 export enum EntityNames {
+  Admin_Tools = "Admin Tools",
   LevelLayout_glb = "LevelLayout.glb",
+  Video_Screen = "Video Screen",
 } 
