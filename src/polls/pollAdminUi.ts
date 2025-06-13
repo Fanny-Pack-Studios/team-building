2d08b20e17b4108c5a6d3c7367ccf4e75ede21e9
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2025-06-13 18:00:52 -0300

    Validate that poll has a title and at least 2 options when creating it

--- src/polls/pollAdminUi.ts
+++ src/polls/pollAdminUi.ts
@@ -1,7 +1,7 @@
 import * as ui from 'dcl-ui-toolkit'
 import { PromptButton } from 'dcl-ui-toolkit/dist/ui-entities/prompts/Prompt/components/Button'
 import { engine, InputAction, MeshCollider, MeshRenderer, pointerEventsSystem, Transform } from '@dcl/sdk/ecs'
-import { Vector3 } from '@dcl/sdk/math'
+import { Vector3, Color4 } from '@dcl/sdk/math'
 import { createPollEntity } from './pollEntity'
 import { PromptInput } from 'dcl-ui-toolkit/dist/ui-entities/prompts/Prompt/components/Input'
 
@@ -11,12 +11,15 @@ type AnswerPrompts = { answerPromptInput: PromptInput, deleteButton: PromptButto
 
 export function createPollAdminUi(): ui.CustomPrompt {
   let answers: string[] = []
-  let questionTitle: string = "Question title"
-  let pollUiHeight = 500
+  let questionTitle: string = ""
+  let pollUiHeight = 550
   let yPosition = pollUiHeight / 2.0
   let answerPrompts: AnswerPrompts = []
   let initialAnswerY = 0
   let addAnswerButton: PromptButton
+  let createButton: PromptButton
+
+  const validAnswers = () => answers.filter(answer => answer && answer.trim() !== '')
 
   const onAnswerPromptsChanged = () => {
     if (addAnswerButton) {
@@ -27,6 +30,7 @@ export function createPollAdminUi(): ui.CustomPrompt {
         addAnswerButton.grayOut()
       }
     }
+
     if (answerPrompts.length > 2) {
       // Only show the delete button in the last option
       answerPrompts[answerPrompts.length - 1].deleteButton.show()
@@ -34,6 +38,19 @@ export function createPollAdminUi(): ui.CustomPrompt {
     } else {
       // In this case, we only have 2 options which is the minimum
       answerPrompts[answerPrompts.length - 1].deleteButton.hide()
+    }
+
+    if (createButton){
+      // Only enable the create button if we have a title and at least 2 answers
+      const hasTitle = questionTitle.trim().length > 0
+
+      const validAnswerCount = validAnswers().length
+  
+      if (hasTitle && validAnswerCount >= 2) {
+        createButton.enable()
+      } else {
+        createButton.grayOut()
+      }
     }
   }
 
@@ -47,6 +64,7 @@ export function createPollAdminUi(): ui.CustomPrompt {
     if (answerPrompts.length === 0) {
       return
     }
+    answers.pop()
     let { answerPromptInput, deleteButton } = answerPrompts.pop()!
     answerPromptInput.hide()
     deleteButton.hide()
@@ -63,6 +81,7 @@ export function createPollAdminUi(): ui.CustomPrompt {
       xPosition: 0,
       onChange: (value: string) => {
         answers[idx] = value
+        onAnswerPromptsChanged()
       }
     })
 
@@ -100,6 +119,7 @@ export function createPollAdminUi(): ui.CustomPrompt {
     xPosition: 0,
     onChange: (value: string) => {
       questionTitle = value
+      onAnswerPromptsChanged()
     }
   })
 
@@ -124,16 +144,27 @@ export function createPollAdminUi(): ui.CustomPrompt {
     onMouseDown: addAnswerPrompt
   })
 
-  createPollPrompt.addButton({
+  const helperText = createPollPrompt.addText({
+    value: 'Add a question and at least 2 options',
+    size: 12,
+    color: Color4.create(0.7, 0.7, 0.7, 1),
+    yPosition: yPosition,
+    xPosition: -50
+  })
+
+  createButton = createPollPrompt.addButton({
     style: ui.ButtonStyles.F,
     text: 'Create',
     yPosition: yPosition,
     xPosition: 100,
     onMouseDown: () => {
-      createPollEntity(questionTitle, answers.slice(0, answerPrompts.length))
+      createPollEntity(questionTitle, validAnswers())
       createPollPrompt.hide()
     }
   })
+  
+  // Initially disable the Create button until we have valid inputs
+  createButton.grayOut()
 
   createPollPrompt.show()
 
