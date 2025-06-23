9988f838f86cc36cfcc4de93236ace52366db47f
agustincarriso <86750198+agustincarriso@users.noreply.github.com>
2025-06-23 08:52:42 -0300

    ft: add stage UI

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,9 @@
 import {} from '@dcl/sdk/math'
 import { UIController } from './ui.controller'
 import { addPollCreator, addPollsSystem } from './polls/poll'
-import { ModeratorTool } from './moderatorTool'
 
 export function main(): void {
   const uiController = new UIController()
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const moderatorTool = new ModeratorTool()
   uiController.start()
   addPollsSystem()
   addPollCreator()
