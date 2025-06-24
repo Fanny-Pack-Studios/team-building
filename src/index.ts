792b31ab98c7ceaadc1c8876be94fb117f02870b
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2025-06-23 21:19:22 -0300

    Add message bus
    Adds an entity with a synced component that lets players broadcast messages to everyone in the scene.
    
    Messages are typed by the schema defined in MessageBusComponent. If new kind of messages are needed, the constant messageContentAlternatives needs to be changed and a new case should be added to the function handleMessage.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-
 // We define the empty imports so the auto-complete feature works as expected.
 import {} from '@dcl/sdk/math'
 import { UIController } from './ui.controller'
 import { addPollCreator, addPollsSystem } from './polls/poll'
+import { setupMessageBus } from './messagebus/messagebus'
 
 export function main(): void {
   const uiController = new UIController()
   uiController.start()
   addPollsSystem()
   addPollCreator()
+
+  setupMessageBus()
 }
--- src/syncEntities.ts
+++ src/syncEntities.ts
@@ -1,4 +1,5 @@
 export enum SyncEntityEnumId {
 	KICK = 1,
-	MODERATOR = 2
+	MODERATOR = 2,
+	MESSAGE_BUS = 3
 }
