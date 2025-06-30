0f67589a57afb4891c4bd4e236776bde905a5921
FranciscoMelink <franciscomelink@gmail.com>
2025-06-30 11:14:02 -0300

    modelos del auditorio

--- assets/scene/entity-names.ts
+++ assets/scene/entity-names.ts
@@ -1,17 +1,18 @@
 // Auto-generated entity names from the scene
 
+
 /**
  * Object containing all entity names in the scene for autocomplete support.
  */
 export enum EntityNames {
-  Admin_Tools = 'Admin Tools',
-  AttendeePanel = 'AttendeePanel',
-  Interactable = 'Interactable',
-  Podium = 'Podium',
-  Ring___Green_Light = 'Ring - Green Light',
-  ShowResultsButton = 'ShowResultsButton',
-  StageWall = 'StageWall',
-  Video_Screen = 'Video Screen',
-  podium_V2_glb = 'podium_V2.glb',
-  voting_zone_glb = 'voting_zone.glb'
-}
+  Admin_Tools = "Admin Tools",
+  AttendeePanel = "AttendeePanel",
+  Interactable = "Interactable",
+  Podium = "Podium",
+  Ring___Green_Light = "Ring - Green Light",
+  ShowResultsButton = "ShowResultsButton",
+  StageWall = "StageWall",
+  Video_Screen = "Video Screen",
+  podium_V2_glb = "podium_V2.glb",
+  voting_zone_glb = "voting_zone.glb",
+} 
