714807644c0641526fc621cb751d29f7b12d9652
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2025-07-02 08:30:00 -0300

    Add customizable parts to auditorium

--- assets/scene/entity-names.ts
+++ assets/scene/entity-names.ts
@@ -1,18 +1,23 @@
 // Auto-generated entity names from the scene
 
-
 /**
  * Object containing all entity names in the scene for autocomplete support.
  */
 export enum EntityNames {
-  Admin_Tools = "Admin Tools",
-  AttendeePanel = "AttendeePanel",
-  Interactable = "Interactable",
-  Podium = "Podium",
-  Ring___Green_Light = "Ring - Green Light",
-  ShowResultsButton = "ShowResultsButton",
-  StageWall = "StageWall",
-  Video_Screen = "Video Screen",
-  podium_V2_glb = "podium_V2.glb",
-  voting_zone_glb = "voting_zone.glb",
-} 
+  Admin_Tools = 'Admin Tools',
+  AttendeePanel = 'AttendeePanel',
+  Interactable = 'Interactable',
+  Podium = 'Podium',
+  Ring___Green_Light = 'Ring - Green Light',
+  ShowResultsButton = 'ShowResultsButton',
+  StageWall = 'StageWall',
+  Video_Screen = 'Video Screen',
+  podium_V2_glb = 'podium_V2.glb',
+  AuditoriumCamera = 'AuditoriumCamera',
+  BannerBackground = 'BannerBackground',
+  BannerBackground_2 = 'BannerBackground_2',
+  BannerLogo = 'BannerLogo',
+  BannerLogo_2 = 'BannerLogo_2',
+  Logo = 'Logo',
+  voting_zone_glb = 'voting_zone.glb'
+}
