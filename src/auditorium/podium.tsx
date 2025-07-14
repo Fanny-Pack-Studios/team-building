caa3307e58e06179dad3e052319799a0bf569528
Agustin <86750198+agustincarriso@users.noreply.github.com>
2025-07-14 13:06:35 -0300

    Add arrow to podium (#139)
    * ft: add target arrow to podium
    
    * fix: update runtime dependencies
    
    * fix: multiplier

--- src/auditorium/podium.tsx
+++ src/auditorium/podium.tsx
@@ -1,12 +1,22 @@
-import { engine, type EventSystemCallback, InputAction, PointerEvents, pointerEventsSystem } from '@dcl/sdk/ecs'
+import {
+  engine,
+  type EventSystemCallback,
+  GltfContainer,
+  InputAction,
+  PointerEvents,
+  pointerEventsSystem,
+  Transform
+} from '@dcl/sdk/ecs'
 import { type GameController } from '../controllers/game.controller'
-import { withPlayerInfo } from '../utils'
+import { BounceComponent, withPlayerInfo } from '../utils'
+import { Vector3 } from '@dcl/sdk/math'
 
 export function setupPodium(gameController: GameController): void {
   const podiumOrNull = engine.getEntityOrNullByName('Podium')
   if (podiumOrNull === null) return
 
   const podium = podiumOrNull
+  const arrow = engine.addEntity()
 
   gameController.hostsController.onChange(() => {
     updatePodiumActions()
@@ -17,10 +27,13 @@ export function setupPodium(gameController: GameController): void {
   function updatePodiumActions(): void {
     withPlayerInfo((player) => {
       if (gameController.hostsController.isHost(player.userId)) {
+        removeArrowFromPodium()
         configurePodiumForHosts()
       } else if (gameController.hostsController.noHostExists()) {
+        addArrowToPodium()
         configurePodiumForClaimingHost()
       } else {
+        removeArrowFromPodium()
         disablePodium()
       }
     })
@@ -50,5 +63,21 @@ export function setupPodium(gameController: GameController): void {
 
   function disablePodium(): void {
     PointerEvents.deleteFrom(podium)
+  }
+
+  function addArrowToPodium(): void {
+    GltfContainer.create(arrow, { src: 'assets/models/target_arrow.glb' })
+    Transform.create(arrow, { position: Vector3.create(8, 3, 9), scale: Vector3.create(1, 1, 1) })
+    BounceComponent.create(arrow, {
+      amplitude: 0.3,
+      speed: 2,
+      offset: 0
+    })
+  }
+
+  function removeArrowFromPodium(): void {
+    GltfContainer.deleteFrom(arrow)
+    BounceComponent.deleteFrom(arrow)
+    Transform.deleteFrom(arrow)
   }
 }
